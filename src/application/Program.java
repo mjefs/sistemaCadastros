c85f84233d22892cb80eb904a49827739f32b18b
Jeferson Nascimento <mjef013@gmail.com>
2025-05-05 19:49:43 -0300

    Leitura do arquivo base com as perguntas essenciais

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,26 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        File formulario = new File("C:/documents/formulario.txt");
+
+        try(BufferedReader br = new BufferedReader(new FileReader(formulario))){
+
+            for(String linha = br.readLine(); linha != null; linha = br.readLine()){
+                System.out.println(linha);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        sc.close();
+    }
 }
